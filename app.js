f8f62f12393d3624f3f5e6c2968af83852d85061
Jarkko <jarkkohei@luukku.com>
2018-03-21 21:06:35 +0200

    Added mongoose, created Product-model and implemented it in the routes/products.js

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://Admin:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@node-rest-shop-shard-00-00-dcwi5.mongodb.net:27017,node-rest-shop-shard-00-01-dcwi5.mongodb.net:27017,node-rest-shop-shard-00-02-dcwi5.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin', 
+
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));

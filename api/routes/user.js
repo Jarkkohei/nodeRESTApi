6091bd30fc1a024ad64b98e8cf2588e7e5829083
Jarkko <jarkkohei@luukku.com>
2018-03-28 18:30:52 +0300

    Added the login-route and the funtionality for it including JWT. #12

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,10 @@
+//import { read } from 'fs';
+
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('./../models/user');
 
@@ -44,6 +47,56 @@ router.post('/signup', (req, res, next) => {
             }
         })
         .catch();
+});
+
+
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email})
+        .exec()
+        .then(user => {
+
+            if(user.length < 1) {
+                return res.status(401).json({
+                    message: 'Authentication failed!'
+                });
+            }
+
+            bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+
+                if(error) {
+                    return res.status(401).json({
+                        message: 'Authentication failed!'
+                    });
+                }
+
+                if(result) {
+                    const token = jwt.sign({
+                                email: user[0].email,
+                                userId: user[0]._id
+                            }, 
+                            process.env.JWT_KEY,
+                            {
+                                expiresIn: "1h"
+                            });
+                            return res.status(200).json({
+                                message: 'Authentication successful',
+                                token: token
+                        }
+                    );
+                }
+
+                return res.status(401).json({
+                    message: 'Authentication failed!'
+                });
+
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
 });
 
 
